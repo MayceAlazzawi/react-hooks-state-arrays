a5bc929497733179b4afe09e31b5f1684b12b5ec
Mayce <mayce@Meenas-MBP.lan>
2022-07-02 11:01:40 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,18 @@
+// import React from "react";
+// import SpicyFoodList from "./SpicyFoodList";
+
+// function App() {
+//   return (
+//     <div>
+//       <h3>SpicyFoodList</h3>
+//       <SpicyFoodList />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from "react";
 import SpicyFoodList from "./SpicyFoodList";
 
--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,29 @@
+// const spicyFoods = [
+//   { id: 1, name: "Buffalo Wings", cuisine: "American", heatLevel: 3 },
+//   { id: 2, name: "Mapo Tofu", cuisine: "Sichuan", heatLevel: 6 },
+// ];
+
+// let nextId = 3;
+
+// const newSpicyFoods = [
+//   { name: "Green Curry", cuisine: "Thai", heatLevel: 9 },
+//   { name: "Enchiladas", cuisine: "Mexican", heatLevel: 2 },
+//   { name: "5 Alarm Chili", cuisine: "American", heatLevel: 5 },
+// ];
+
+// // Get one random spicy food from the array
+// function getNewRandomSpicyFood() {
+//   const index = Math.floor(Math.random() * newSpicyFoods.length);
+//   const newSpicyFood = { ...newSpicyFoods[index] };
+//   newSpicyFood.id = nextId;
+//   nextId++;
+//   return newSpicyFood;
+// }
+
+// export { spicyFoods, getNewRandomSpicyFood };
+
+
+
 const spicyFoods = [
   { id: 1, name: "Buffalo Wings", cuisine: "American", heatLevel: 3 },
   { id: 2, name: "Mapo Tofu", cuisine: "Sichuan", heatLevel: 6 },
@@ -12,7 +38,7 @@ const newSpicyFoods = [
 ];
 
 // Get one random spicy food from the array
-function getNewRandomSpicyFood() {
+function randomSpicyFood() {
   const index = Math.floor(Math.random() * newSpicyFoods.length);
   const newSpicyFood = { ...newSpicyFoods[index] };
   newSpicyFood.id = nextId;
@@ -20,4 +46,8 @@ function getNewRandomSpicyFood() {
   return newSpicyFood;
 }
 
-export { spicyFoods, getNewRandomSpicyFood };
+function getNewSpicyFood() {
+  return randomSpicyFood();
+}
+
+export { spicyFoods, getNewSpicyFood };
